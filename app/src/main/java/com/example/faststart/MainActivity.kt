64a14df805509793efd4f7d7a10c843ad11375a3
HanDroiD <super.lukianchuk2010@gmail.com>
2021-12-22 01:18:34 +0200

    added dependency= @Retrofit && @RxJava && @Room && @LifeData && @ViewModel && @Picasso && created CoinInfo.kt

--- app/src/main/java/com/example/faststart/MainActivity.kt
+++ app/src/main/java/com/example/faststart/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.faststart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private var time:String = "00:00:00"
+    private var isRunState = false
+    private var referenceTime = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editText = findViewById(R.id.et_timer_scoreboard)
+        val receiveText = editText.text.toString()
     }
 }
