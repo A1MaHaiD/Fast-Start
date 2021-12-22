a6016484bee02746739d6e4b136a90680ed7271f
HanDroiD <super.lukianchuk2010@gmail.com>
2021-12-22 14:38:13 +0200

    created CoinInfoListOfData.kt && CoinPriceInfo.kt && CoinPriceInfoRawData.kt && Datum.kt

---
+++ app/src/main/java/com/example/faststart/pojo/Datum.kt
@@ -0,0 +1,43 @@
+package com.example.faststart.pojo
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Datum (
+    @SerializedName("CoinInfo")
+    @Expose
+    var coinInfo: CoinInfo? = null,
+
+/*    @SerializedName("RAW")
+    @Expose
+    private var raw: Raw? = null,
+
+    @SerializedName("DISPLAY")
+    @Expose
+    private var display: Display? = null*/
+
+)
+/*fun getCoinInfo(): CoinInfo? {
+    return coinInfo
+}
+
+fun setCoinInfo(coinInfo: CoinInfo?) {
+    this.coinInfo = coinInfo
+}
+
+fun getRaw(): Raw? {
+    return raw
+}
+
+fun setRaw(raw: Raw?) {
+    this.raw = raw
+}
+
+fun getDisplay(): Display? {
+    return display
+}
+
+fun setDisplay(display: Display?) {
+    this.display = display
+}*/
+
