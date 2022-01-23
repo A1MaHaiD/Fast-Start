81c4f43c8ab59344740d6d50be5bad7979d32fc2
Alexandr <super.lukianchuk2010@gmail.com>
2022-01-23 06:20:37 +0200

    added activity_coin_price_list.xml (land) && fragment_coin_detail.xml inside activity_coin_price_list.xml

--- app/src/main/java/com/example/cryptoapp/presenter/ui/activities/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presenter/ui/activities/CoinPriceListActivity.kt
@@ -1,31 +1,33 @@
 package com.example.cryptoapp.presenter.ui.activities
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.ActivityCoinPriceListBinding
 import com.example.cryptoapp.domain.entity.CoinInfoEntity
 import com.example.cryptoapp.presenter.ui.adapters.CoinInfoAdapter
+import com.example.cryptoapp.presenter.ui.fragments.CoinDetailFragment
 import com.example.cryptoapp.presenter.view_models.CoinViewModel
 
 
 class CoinPriceListActivity : AppCompatActivity() {
+
     private lateinit var viewModel: CoinViewModel
     private val binding by lazy {
         ActivityCoinPriceListBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
-            override fun onCoinClick(coinInfoDTO: CoinInfoEntity) {
-                CoinDetailActivity.newIntent(
-                    this@CoinPriceListActivity,
-                    coinInfoDTO.fromSymbol
-                ).apply {
-                    startActivity(intent)
+            override fun onCoinClick(coinInfo: CoinInfoEntity) {
+                if (isOnePaneMode()) {
+                    launchDetailActivity(coinInfo.fromSymbol)
+                } else {
+                    launchDetailFragment(coinInfo.fromSymbol)
                 }
             }
         }
@@ -35,5 +37,25 @@ class CoinPriceListActivity : AppCompatActivity() {
         viewModel.coinInfoList.observe(this) {
             adapter.submitList(it)
         }
+    }
+
+    private fun isOnePaneMode() = binding.fragmentContainer == null
+
+
+    private fun launchDetailActivity(fromSymbol: String) {
+        val intent = CoinDetailActivity.newIntent(
+            this@CoinPriceListActivity,
+            fromSymbol
+        )
+        startActivity(intent)
+    }
+
+    private fun launchDetailFragment(fromSymbol: String) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+            .addToBackStack(null)
+            .commit()
     }
 }
